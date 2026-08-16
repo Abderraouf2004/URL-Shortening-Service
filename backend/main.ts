2f7183112755a618336e4b37537f74ae82512ab1
Abderraouf Kallouche <kaabdo2004@gmail.com>
2026-08-16 14:29:41 +0100

    fix: prepare backend for production deployment

--- backend/main.ts
+++ backend/main.ts
@@ -8,6 +8,6 @@ app.use('/api', router);
 
 const PORT = process.env.PORT || 3000;      
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
